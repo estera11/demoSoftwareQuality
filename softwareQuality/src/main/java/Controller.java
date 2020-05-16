5247d1f6cf4a41367626d8cfe593e13db0b53686
Estera Corbu <corbu.georgiana98@gmail.com>
2020-05-16 22:48:22 +0100

    Refactor adding questions to  survey

--- softwareQuality/src/main/java/Controller.java
+++ softwareQuality/src/main/java/Controller.java
@@ -1,31 +1,82 @@
 import model.Question;
 import model.Survey;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+    private static Survey survey;
+    private static Survey survey2;
+    private static  List<Survey> surveys = new ArrayList<>();
+
+
     Controller(){}
 
-    public String createSurvey(String title){
-        Survey survey = new Survey(title);
-        System.out.println(survey.getTitle());
-        return survey.getTitle();
+    public Survey createSurvey(String title){
+       Survey survey = new Survey(title);
+        return survey;
     }
 
-    public int addQuestion(String surveyTitle){
-        Question question1 = new Question(1,"Service Quality");
-        Question question2 = new Question(2,"Cleanliness");
-        Question question3 = new Question(3,"Comfort");
+    public void addQuestionToSurvey(Question question, Survey s){
+        if(s.getQuestionList().size()<10) {
+            s.addQuestion(question);
+        }
+        else{
+            System.out.println("Question number limit reached.");
+        }
 
-        Survey survey = new Survey(surveyTitle);
-        survey.addQuestion(question1);
-        survey.addQuestion(question2);
-        survey.addQuestion(question3);
+    }
 
-        return survey.getQuestionList().size();
+    public String displaySurveys(List<Survey> surveys){
+        String response = "";
+        if(surveys.size()==0){
+            response = "No survey to display";
+        }else{
+            response = "There are "+surveys.size()+" surveys";
+            for(Survey s : surveys){
+                System.out.println("Survey Title: "+s.getTitle());
+                for (Question q: s.getQuestionList()) {
+                    System.out.println("Question: "+q.getQuestion());
+                }
+                System.out.println("");
+            }
+        }
+       return response;
     }
 
     public static void main(String[] args) {
         Controller c = new Controller();
-        c.createSurvey("Service Quality");
+
+        //create surveys and display their titles
+        survey = c.createSurvey("First Survey");
+        System.out.println("Survey created with title: "+survey.getTitle());
+        System.out.println("");
+
+        survey2 = c.createSurvey("Second Survey");
+        System.out.println("Survey created with title: "+survey2.getTitle());
+        System.out.println("");
+
+
+        //creating the questions and adding them to the survey
+        Question q1 = new Question(1,"Service Quality");
+        Question q2 = new Question(2, "Cleanliness");
+        Question q3 = new Question(3, "Comfort");
+        Question q4 = new Question(4, "Facilities");
+        Question q5 = new Question(5, "Service Diversification");
+
+        c.addQuestionToSurvey(q1, survey);
+        c.addQuestionToSurvey(q2, survey);
+        c.addQuestionToSurvey(q3, survey);
+
+
+        c.addQuestionToSurvey(q4, survey2);
+        c.addQuestionToSurvey(q5, survey2);
+
+        //display the surveys
+        surveys.add(survey);
+        surveys.add(survey2);
+
+        c.displaySurveys(surveys);
     }
 
 }
--- softwareQuality/src/test/java/ControllerTest.java
+++ softwareQuality/src/test/java/ControllerTest.java
@@ -1,5 +1,10 @@
 
+import model.Question;
+import model.Survey;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -10,15 +15,49 @@ public class ControllerTest {
     @Test
     public void testCreateSurvey() {
         controller.createSurvey("First Survey");
-        assertEquals("First Survey", controller.createSurvey("First Survey"));
+        assertEquals("First Survey", controller.createSurvey("First Survey").getTitle());
     }
 
     @Test
     public void testAddQuestion(){
-        int expected = 3;
-        int actual = controller.addQuestion("First Survey");
+        Question q1 = new Question(1,"Service Quality");
+        Question q2 = new Question(2,"Comfort");
+
+        Survey survey = new Survey("First Survey");
+        controller.addQuestionToSurvey(q1, survey);
+        controller.addQuestionToSurvey(q2, survey);
+        int expected = 2;
+        int actual = survey.getQuestionList().size();
+
         assertEquals(expected, actual);
     }
 
-    
+    @Test
+    public void testDisplaySurveys(){
+        Survey survey = new Survey("First Survey");
+        Question q = new Question(1,"Service Quality");
+        survey.addQuestion(q);
+
+        Survey survey2 = new Survey("Second Survey");
+        Question question = new Question(1, "Cleanliness");
+        survey2.addQuestion(question);
+
+        Survey survey3= new Survey("Third Survey");
+        Question q2 = new Question(1, "Comfort");
+        survey3.addQuestion(q2);
+
+
+        List<Survey> surveys = new ArrayList<>();
+        surveys.add(survey);
+        surveys.add(survey2);
+        surveys.add(survey3);
+
+        String expected = "There are 3 surveys";
+        String actual = controller.displaySurveys(surveys);
+
+        assertEquals(expected, actual);
+
+    }
+
+
 }
